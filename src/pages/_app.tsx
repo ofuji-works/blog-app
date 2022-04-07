e8a2fa645eb9b3df3f7428c87cd94972ba54aab0
ofuji-works <ofuji.works@gmail.com>
2022-04-08 02:07:11 +0900

    recoil導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { RecoilRoot } from "recoil";
+
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
-export default MyApp
+export default MyApp;
