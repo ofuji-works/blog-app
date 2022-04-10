c385d85e6d290cbe539f03dd520efe58443d254b
ofuji-works <ofuji.works@gmail.com>
2022-04-11 01:50:31 +0900

    chakra ui 設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
+
+// テーマカラー設定
+const colors = {}
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </RecoilRoot>
   )
 }
