2bb82febc086993d15dfb95629780e1ab4542a18
ofuji-works <ofuji.works@gmail.com>
2022-05-05 03:05:40 +0900

    useBreakPoints

---
+++ src/hooks/useBreakPoints.ts
@@ -0,0 +1,14 @@
+import { useMediaQuery } from '@chakra-ui/react'
+
+import { BREAK_POINTS } from '@/config/index'
+
+/**
+ * 現在の表示デバイスの判断 by breakpoints
+ * @returns {object}
+ * @property {boolean} isSP スマートフォン表示フラグ
+ * @property {boolean} isTablet タブレット表示フラグ
+ */
+export const useBreakPoints = () => {
+  const [isSP, isTablet] = useMediaQuery([`(max-width: ${BREAK_POINTS.sm})`, `(max-width: ${BREAK_POINTS.md})`])
+  return { isSP, isTablet }
+}
