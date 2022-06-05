5338794bdbdd2820ac70b4b4aa6821e0b2c80b97
ofuji-works <ofuji.works@gmail.com>
2022-06-06 01:57:00 +0900

    Key Visual Component

---
+++ src/components/KeyVisual/index.ts
@@ -0,0 +1,1 @@
+export { KeyVisual } from './KeyVisual'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 
 import { BlogList, SearchTagLinks, Profile } from '@/features'
-import { TwoColumnContainer } from '@/components'
+import { TwoColumnContainer, KeyVisual } from '@/components'
 
 const categories = ['React']
 const tags = ['React']
@@ -10,6 +10,7 @@ const items = ['React']
 const Home: NextPage = () => {
   return (
     <>
+      <KeyVisual />
       <TwoColumnContainer LeftComponent={() => <BlogList items={items} />} RightComponent={() => <Profile />} />
       <SearchTagLinks categories={categories} tags={tags} />
     </>
