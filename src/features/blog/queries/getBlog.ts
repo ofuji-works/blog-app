410c1349211ef5e9b43b9f801f97abb8d2fce00f
ofuji-works <ofuji.works@gmail.com>
2022-06-13 01:28:29 +0900

    Query

--- src/features/blog/queries/getBlog.ts
+++ src/features/blog/queries/getBlog.ts
@@ -2,11 +2,50 @@ import { gql } from '@apollo/client'
 
 import type { Document } from '@contentful/rich-text-types'
 
+export interface ArticleTextLinks {
+  assets: {
+    block: ArticleTextAsset[]
+  }
+  entries: {
+    block: EmbeddedEntryBlock[]
+    inline: EmbeddedEntryInline[]
+  }
+}
+export interface ArticleTextAsset {
+  sys: {
+    id: string
+  }
+  title: string
+  description: string
+  url: string
+}
+
+export interface EmbeddedEntryBlock {
+  sys: {
+    id: string
+  }
+  title: string
+  body: {
+    json: Document
+  }
+}
+
+export interface EmbeddedEntryInline {
+  sys: {
+    id: string
+  }
+  title: string
+}
+
 export interface BlogQuery {
   blog: {
+    sys: {
+      publishedAt: string
+    }
     title: string
     body: {
       json: Document
+      links: ArticleTextLinks
     }
   }
 }
@@ -14,9 +53,45 @@ export interface BlogQuery {
 export const GET_BLOG_QUERY = gql`
   query ($id: String!, $preview: Boolean = false) {
     blog(id: $id, preview: $preview) {
+      sys {
+        publishedAt
+      }
       title
       body {
         json
+        links {
+          assets {
+            block {
+              sys {
+                id
+              }
+              title
+              description
+              url
+            }
+          }
+          entries {
+            block {
+              sys {
+                id
+              }
+              ... on Blog {
+                title
+                body {
+                  json
+                }
+              }
+            }
+            inline {
+              sys {
+                id
+              }
+              ... on Blog {
+                title
+              }
+            }
+          }
+        }
       }
     }
   }
--- src/features/blog/queries/index.ts
+++ src/features/blog/queries/index.ts
@@ -1,4 +1,4 @@
 export { GET_BLOGS_QUERY } from './getBlogs'
 export type { BlogsQuery } from './getBlogs'
 export { GET_BLOG_QUERY } from './getBlog'
-export type { BlogQuery } from './getBlog'
+export type { ArticleTextAsset, ArticleTextLinks, BlogQuery, EmbeddedEntryBlock, EmbeddedEntryInline } from './getBlog'
