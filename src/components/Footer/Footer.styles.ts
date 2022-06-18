844b78a137a56cac41db3947dc9f0a420fb82365
ofuji-works <ofuji.works@gmail.com>
2022-06-19 02:43:11 +0900

    Footer実装

---
+++ src/components/Footer/Footer.styles.ts
@@ -0,0 +1,9 @@
+import { chakra, Text } from '@chakra-ui/react'
+
+export const Copyright = chakra(Text, {
+  baseStyle: {
+    textAlign: 'center',
+    marginTop: 1,
+    marginBottom: 1,
+  },
+})
