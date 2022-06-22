e98c46abbff99371014949381dd4b5027d179fa7
ofuji-works <ofuji.works@gmail.com>
2022-06-23 02:13:39 +0900

    add 404

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import type { AppProps } from 'next/app'
 import { RootProvider } from '@/providers'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+    const mockStart = () => import('../../.mock')
+    mockStart()
+  }
+
   return (
     <RootProvider pageProps={pageProps}>
       <Component {...pageProps} />
