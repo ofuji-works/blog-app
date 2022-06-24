36b0e38a65f1287c60a354cd57cd3cec424841e2
ofuji-works <ofuji.works@gmail.com>
2022-06-24 09:24:13 +0900

    e2eでのmsw採用をやめる

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,6 @@ import type { AppProps } from 'next/app'
 import { RootProvider } from '@/providers'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-    const mockStart = () => import('../../.mock')
-    mockStart()
-  }
-
   return (
     <RootProvider pageProps={pageProps}>
       <Component {...pageProps} />
