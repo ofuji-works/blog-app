d0e15f9c5747c9438fae7d630512173f87002e5c
ofuji-works <ofuji.works@gmail.com>
2022-07-03 07:28:38 +0900

    bundle-analyzer 導入

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+const withPlugins = require('next-compose-plugins')
 
-const { withSentryConfig } = require('@sentry/nextjs')
+// bundle analyzer
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const moduleExports = {
   // Your existing module.exports
   reactStrictMode: true,
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
 }
 
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+const { withSentryConfig } = require('@sentry/nextjs')
 const sentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
   // the following options are set automatically, and overriding them is not
@@ -24,4 +32,4 @@ const sentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
+module.exports = withPlugins([[withSentryConfig, sentryWebpackPluginOptions], withBundleAnalyzer], moduleExports)
