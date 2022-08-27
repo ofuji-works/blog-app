0402f16966d8facfb7c2ee2cdb0a2ea654d22512
ofuji-works <ofuji.works@gmail.com>
2022-08-27 11:56:04 +0900

    chore: storybook-addon-next

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,11 +4,22 @@ const toPath = (_path) => path.join(process.cwd(), _path)
 module.exports = {
   typescript: { reactDocgen: false },
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: 'storybook-addon-next',
+      options: {
+        nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+      },
+    },
+  ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
   },
+  staticDirs: ['../public'],
   webpackFinal: async (config) => {
     return {
       ...config,
@@ -18,6 +29,7 @@ module.exports = {
           ...config.resolve.alias,
           '@emotion/core': toPath('node_modules/@emotion/react'),
           'emotion-theming': toPath('node_modules/@emotion/react'),
+          '@/mock': path.resolve(__dirname, '../__tests__/mock'),
           '@': path.resolve(__dirname, '../src'),
         },
       },
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,12 @@
+import React from 'react'
+import NextImage from 'next/image'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../src/assets/index.styles'
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <img {...props} />,
+})
 
 const withChakra = (StoryFn: Function) => {
   return (
