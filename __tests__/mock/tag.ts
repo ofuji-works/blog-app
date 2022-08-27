e647c48e5c9b228e7600e7d9ebd2f1d394a83ab0
ofuji-works <ofuji.works@gmail.com>
2022-08-27 13:48:30 +0900

    test: tag mock 分割

--- __tests__/mock/tag.ts
+++ __tests__/mock/tag.ts
@@ -1,5 +1,9 @@
 import { TagsQuery } from '@/features'
 
+export const tagMock = { id: 'tag', name: 'Tag' }
+
+export const tagsMock = new Array(5).fill(tagMock)
+
 export const tagsQueryMock: TagsQuery = {
-  tagCollection: { items: [{ contentfulMetadata: { tags: [{ id: 'tag', name: 'Tag' }] } }] },
+  tagCollection: { items: [{ contentfulMetadata: { tags: tagsMock } }] },
 }
