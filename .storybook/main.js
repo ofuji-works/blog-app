9af46202e8e388d0a531fea46dfc736443d1aa0d
ofuji-works <ofuji.works@gmail.com>
2022-09-04 01:31:00 +0900

    feat: storybook play

--- .storybook/main.js
+++ .storybook/main.js
@@ -15,6 +15,9 @@ module.exports = {
       },
     },
   ],
+  features: {
+    interactionsDebugger: true,
+  },
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
