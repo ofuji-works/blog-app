0e8cd9ad9cd4ce4165440b4b249ffb9fad094243
ofuji-works <ofuji.works@gmail.com>
2022-09-08 02:51:59 +0900

    test: mockデータ更新

--- __tests__/mock/tag.ts
+++ __tests__/mock/tag.ts
@@ -1,8 +1,13 @@
 import { TagsQuery } from '@/features'
 
-export const tagMock = { id: 'tag', name: 'Tag' }
-
-export const tagsMock = new Array(5).fill(tagMock)
+export const tagsMock = [
+  { id: 'tag1', name: 'Tag1' },
+  { id: 'tag10', name: 'Tag10' },
+  { id: 'tag100', name: 'Tag100' },
+  { id: 'tag1000', name: 'Tag1000' },
+  { id: 'tag10000', name: 'Tag10000' },
+  { id: 'tag100000', name: 'Tag100000' },
+]
 
 export const tagsQueryMock: TagsQuery = {
   tagCollection: { items: [{ contentfulMetadata: { tags: tagsMock } }] },
