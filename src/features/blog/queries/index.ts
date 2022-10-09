7b6a5d3b536b281dcf6360c002750d8ebe481a3a
ofuji-works <ofuji.works@gmail.com>
2022-10-09 12:54:12 +0900

    refactor:entry point export のワイルド化

--- src/features/blog/queries/index.ts
+++ src/features/blog/queries/index.ts
@@ -1,4 +1,2 @@
-export { GET_BLOGS_QUERY } from './getBlogs'
-export type { BlogsQuery } from './getBlogs'
-export { GET_BLOG_QUERY } from './getBlog'
+export * from './getBlogs'
 export type { ArticleTextAsset, ArticleTextLinks, BlogType, BlogQuery } from './types'
