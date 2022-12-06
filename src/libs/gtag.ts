fbd14b705897daa653965fa9e52eec2dd43b19ea
ofuji-works <ofuji.works@gmail.com>
2022-12-07 01:16:23 +0900

    refactor: ソースのコメントアウト

--- src/libs/gtag.ts
+++ src/libs/gtag.ts
@@ -3,25 +3,25 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview: (url: string) => void = (url) => {
-  if (window !== undefined) {
-    if (!GA_TRACKING_ID) {
-      console.error('undefined GA_TRACKING_ID')
-      return
-    }
-    window.gtag('config', GA_TRACKING_ID, {
-      page_path: url,
-    })
-  }
-}
+// export const pageview: (url: string) => void = (url) => {
+//   if (window !== undefined) {
+//     if (!GA_TRACKING_ID) {
+//       console.error('undefined GA_TRACKING_ID')
+//       return
+//     }
+//     window.gtag('config', GA_TRACKING_ID, {
+//       page_path: url,
+//     })
+//   }
+// }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }: { [key: string]: string }) => {
-  if (window !== undefined) {
-    window.gtag('event', action, {
-      event_category: category,
-      event_label: label,
-      value: value,
-    })
-  }
-}
+// export const event = ({ action, category, label, value }: { [key: string]: string }) => {
+//   if (window !== undefined) {
+//     window.gtag('event', action, {
+//       event_category: category,
+//       event_label: label,
+//       value: value,
+//     })
+//   }
+// }
