9de5eb8c063fef1c24d345da838f76d3fe6da384
ofuji-works <ofuji.works@gmail.com>
2022-12-26 22:37:13 +0900

    test: Storybookの背景addonの追加

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import NextImage from 'next/image'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Container } from '@chakra-ui/react'
 import theme from '../src/theme'
+
+import { Parameters } from '@storybook/react'
 
 Object.defineProperty(NextImage, 'default', {
   configurable: true,
   value: (props) => <img {...props} />,
 })
+
+export const parameters = {
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: '#FAFAFA',
+      },
+    ],
+  },
+}
 
 const withChakra = (StoryFn: Function) => {
   return (
