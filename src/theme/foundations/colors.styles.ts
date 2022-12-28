02fa9644c3f3b7b633c38527563663d9699c4c73
ofuji-works <ofuji.works@gmail.com>
2022-12-28 16:29:14 +0900

    style: 利用していないカスタムthemeの削除

--- src/theme/foundations/colors.styles.ts
+++ src/theme/foundations/colors.styles.ts
@@ -1,20 +1,3 @@
-import { COLORS } from '@/config'
-
-// テーマカラー設定
-const colors = {
-  black: COLORS.black,
-  gray: {
-    50: COLORS.gray[50],
-    100: COLORS.gray[100],
-    150: COLORS.gray[150],
-    200: COLORS.gray[200],
-  },
-  white: COLORS.white,
-  code: COLORS.code,
-  thumnail: COLORS.thumnail,
-  green: COLORS.green,
-  orange: COLORS.orange,
-  red: COLORS.red,
-}
+const colors = {}
 
 export default colors
--- src/theme/foundations/typography.styles.ts
+++ src/theme/foundations/typography.styles.ts
@@ -1,38 +1,10 @@
 const typography = {
   /* typography */
-  fonts: {
-    code: "'Space Mono', monospace;",
-  },
-  fontSizes: {
-    xs: '0.75rem',
-    sm: '0.875rem',
-    md: '1rem',
-    lg: '1.25rem',
-    xl: '1.5rem',
-    '2xl': '2rem',
-    '3xl': '3rem',
-    '4xl': '4rem',
-    /* blog style */
-    h1: '1.75rem',
-    h2: '1.5rem',
-    p: '1rem',
-  },
-  fontWeight: {
-    bold: '700',
-  },
-  lineHeights: {
-    normal: 'normal',
-    none: 1,
-    base: '1.25rem',
-    /* blog style */
-    h1: '2em',
-    h2: '1.8rem',
-    p: '1.50rem',
-  },
-  letterSpacing: {
-    normal: '0',
-    wide: '0.05em',
-  },
+  fonts: {},
+  fontSizes: {},
+  fontWeight: {},
+  lineHeights: {},
+  letterSpacing: {},
 }
 
 export default typography
