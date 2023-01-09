9c397f0308fcb4ed06b947c654e8313ed7b552ce
ofuji-works <ofuji.works@gmail.com>
2023-01-09 15:01:57 +0900

    refactor: 不要ファイルの削除

--- src/features/blog/BlogList.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentProps, FC } from 'react'
-
-import { EnhancedBlogList } from './container'
-
-type Props = ComponentProps<typeof EnhancedBlogList>
-
-const BlogList: FC<Props> = (props) => <EnhancedBlogList {...props} />
-
-export default BlogList
--- src/features/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './blog'
-export * from './profile'
-export * from './tag_links'
