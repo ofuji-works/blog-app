36d1a3effbf9f9490e7b8129385da57629902c41
ofuji-works <ofuji.works@gmail.com>
2023-01-14 23:51:48 +0900

    style: Listの修正

--- src/features/blog/components/List/List.tsx
+++ src/features/blog/components/List/List.tsx
@@ -1,10 +1,9 @@
 import type { FC } from 'react'
 
 import Link from 'next/link'
-import { HStack, Heading, Stack, VStack } from '@chakra-ui/react'
+import { Heading, Stack, VStack, Wrap, WrapItem } from '@chakra-ui/react'
 import { MdCode } from 'react-icons/md'
 
-import { useBreakPoints } from '@/hooks'
 import { Image, Tag } from '@/components'
 
 type Props = {
@@ -23,59 +22,59 @@ type Props = {
 }
 
 export const List: FC<Props> = ({ items }) => {
-  const { isSP } = useBreakPoints()
   return (
-    <Stack
+    <Wrap
       maxW={{
         sm: '900px',
       }}
-      direction={{
-        base: 'column',
-        sm: 'row',
-      }}
-      flexWrap={{
-        base: 'unset',
-        sm: 'wrap',
-      }}
-      gap={{
+      justifyContent="space-between"
+      spacing={{
         base: '4',
         sm: '6',
       }}
     >
-      {items.map(({ thumnail, title, tags }) => (
-        <VStack key={title} style={{ margin: 0 }} width={{ base: '100%', sm: 'calc(100% / 3)' }}>
-          <Stack
-            width="100%"
-            direction={{
-              base: 'row',
-              sm: 'column',
-            }}
-          >
-            <Image
-              style={{
-                aspectRatio: '4 / 3',
+      {items.map(({ thumnail, title, tags, href }) => (
+        <WrapItem key={title} width={{ base: '100%', sm: 'calc(100% / 3 - 24px)' }}>
+          <VStack alignItems="flex-start" width="100%">
+            <Stack
+              width="100%"
+              direction={{
+                base: 'row',
+                sm: 'column',
               }}
-              width={{
-                base: '25%',
-                sm: '100%',
-              }}
-              src={thumnail.url}
-              alt={thumnail.title}
-              blurDataURL={`${thumnail.url}?q=1`}
-            />
-            <Link as="a" href="/">
-              <Heading size="xs">{title}</Heading>
-            </Link>
-          </Stack>
-          <HStack width="100%" justify="flex-start" flexWrap="wrap">
-            {tags.map((tag) => (
-              <Tag key={tag.id} variant="forBlog" LeftIcon={MdCode} size="sm">
-                {tag.name}
-              </Tag>
-            ))}
-          </HStack>
-        </VStack>
+            >
+              <Image
+                boxShadow={{
+                  base: '0 0.05rem 0.075rem rgba(0, 0, 0, 0.25)',
+                  sm: '0 0.15rem 0.075rem rgba(0, 0, 0, 0.25)',
+                }}
+                style={{
+                  aspectRatio: '4 / 3',
+                }}
+                minWidth={{
+                  base: '25%',
+                  sm: '100%',
+                }}
+                src={thumnail.url}
+                alt={thumnail.title}
+                blurDataURL={`${thumnail.url}?q=1`}
+              />
+              <Link href={href}>
+                <Heading size="xs">{title}</Heading>
+              </Link>
+            </Stack>
+            <Wrap justify="flex-start" spacing="1">
+              {tags.map((tag) => (
+                <WrapItem key={tag.id}>
+                  <Tag variant="forBlog" LeftIcon={MdCode} size="sm">
+                    {tag.name}
+                  </Tag>
+                </WrapItem>
+              ))}
+            </Wrap>
+          </VStack>
+        </WrapItem>
       ))}
-    </Stack>
+    </Wrap>
   )
 }
