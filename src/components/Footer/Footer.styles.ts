2483fe1ee439e1aebaca0ee06bb17a69e3265e12
ofuji-works <ofuji.works@gmail.com>
2023-01-14 23:58:38 +0900

    不要となったComponentの削除

--- src/components/Footer/Footer.styles.ts
+++
@@ -1,9 +0,0 @@
-import { chakra, Text } from '@chakra-ui/react'
-
-export const Copyright = chakra(Text, {
-  baseStyle: {
-    textAlign: 'center',
-    marginTop: 1,
-    marginBottom: 1,
-  },
-})
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,4 @@
 export * from './Breadcrumb'
 export * from './Pager'
-export * from './Footer'
-export * from './Container'
-export * from './Header'
-export * from './Layout'
 
 export * from './ui'
