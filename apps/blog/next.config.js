881602e117a54767f22e5e8888db62599e9e8ee8
ofuji-works <ofuji.works@gmail.com>
2023-03-02 04:37:04 +0900

    fix: nextConfig修正

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,11 +1,14 @@
-const withPlugins = require('next-compose-plugins')
-
 // bundle analyzer
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
 const withT = require('next-transpile-modules')(['@packages/ui'])
+
+const { withSentryConfig } = require('@sentry/nextjs')
+const sentryWebpackPluginOptions = {
+  silent: true,
+}
 
 /**
  * @type {import("next").NextConfig}
@@ -29,30 +32,12 @@ const moduleExports = {
   },
 }
 
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-const { withSentryConfig } = require('@sentry/nextjs')
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
+module.exports = () => {
+  const plugins = [withT, withSentryConfig, withBundleAnalyzer]
+  return plugins.reduce((acc, next) => {
+    if (next.name === 'withSentryConfig') {
+      return next(acc, sentryWebpackPluginOptions)
+    }
+    return next(acc)
+  }, moduleExports)
 }
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withPlugins(
-  [
-    (nextConfig) => withSentryConfig(nextConfig, sentryWebpackPluginOptions),
-    (nextConfig) => withT(nextConfig),
-    withBundleAnalyzer,
-  ],
-  moduleExports,
-)
