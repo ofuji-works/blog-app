50835ce67cf0e634108e6aec0300b5fb3317a5ff
ofuji-works <ofuji.works@gmail.com>
2023-03-30 00:58:40 +0900

    fix: remove comment & isProduction

--- apps/blog/src/libs/sentry.ts
+++ apps/blog/src/libs/sentry.ts
@@ -1,14 +1,8 @@
 import * as Sentry from '@sentry/nextjs'
 
-const isProduction = () => process.env.NODE_ENV === 'production'
-
 export const captureExceptionForSentry = (err: Error) => {
-  if (isProduction()) {
-    Sentry.captureException(err)
-  }
+  Sentry.captureException(err)
 }
 export const flushForSentry = (timeout: number) => {
-  if (isProduction()) {
-    Sentry.flush(timeout)
-  }
+  Sentry.flush(timeout)
 }
