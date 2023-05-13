99b38ef3fc2603cdc9181099c9bfa0130914de95
ofuji-works <ofuji.works@gmail.com>
2023-05-13 16:54:57 +0900

    feat: remove media

--- packages/ui/src/components/media/avatar/Avatar.tsx
+++
@@ -1,5 +0,0 @@
-import type { FC } from 'react'
-
-import { Avatar as ChakraAvatar, AvatarProps } from '@chakra-ui/react'
-
-export const Avatar: FC<AvatarProps> = (props) => <ChakraAvatar {...props} />
